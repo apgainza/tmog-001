99ee0ac0b76765cbbd0edd9363996a40fc3990b8
Alejandro Portilla Gainza <aportiga@mercadona.es>
2024-02-10 20:48:32 +0100

    feat: Two sum

--- src/main/java/org/tfoc/Solution.java
+++ src/main/java/org/tfoc/Solution.java
@@ -1,15 +1,43 @@
 package org.tfoc;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * The class containing the solution to this exercise
  */
 public class Solution {
 
-    public int[] twoSum(
-            int[] nums,
-            int target) {
-
-        return null;
+    public static int[] twoSum(int[] nums, int target) {
+        int[] response = new int[]{};
+        Map<Integer, Integer> mapNums = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int res = target - nums[i];
+            if (mapNums.containsKey(res)) {
+                response = new int[]{mapNums.get(res), i};
+                break;
+            }
+            mapNums.put(nums[i], i);
+        }
+        return response;
     }
 
+    public static int[] twoSumWorse(int[] nums, int target) {
+        int[] response = new int[]{};
+        for (int i = 0; i < nums.length; i++) {
+            boolean isTargetFound = false;
+            for (int j = i + 1; j < nums.length; j++) {
+                Integer firstParemeter = nums[i];
+                Integer secondParameter = nums[j];
+                if (target == firstParemeter + secondParameter) {
+                    response = new int[]{i, j};
+                    isTargetFound = true;
+                    break;
+                }
+            }
+            if (isTargetFound)
+                break;
+        }
+        return response;
+    }
 }
--- src/test/java/org/tfoc/SolutionTest.java
+++ src/test/java/org/tfoc/SolutionTest.java
@@ -1,15 +1,41 @@
 package org.tfoc;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 @Slf4j
 class SolutionTest {
 
-    @Test
-    void testSolution() {
-
-        log.info("Don't forget to write the tests to make sure it works. Enjoy!");
+    private static Stream<Arguments> provideInputNums() {
+        return Stream.of(
+                Arguments.of(new int[]{2, 7, 11, 15}, 9, new int[]{0, 1}),
+                Arguments.of(new int[]{3, 2, 4}, 6, new int[]{1, 2}),
+                Arguments.of(new int[]{3, 3}, 6, new int[]{0, 1}),
+                Arguments.of(new int[]{1, 2, 3}, 6, new int[]{})
+        );
     }
+
+    @ParameterizedTest
+    @MethodSource("provideInputNums")
+    void twoSum(int[] nums, Integer target, int[] responseExpected) {
+        int[] response = Solution.twoSum(nums, target);
+
+        assertArrayEquals(responseExpected, response);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideInputNums")
+    void twoSumWorse(int[] nums, Integer target, int[] responseExpected) {
+        int[] response = Solution.twoSumWorse(nums, target);
+
+        assertArrayEquals(responseExpected, response);
+    }
+
 
 }
